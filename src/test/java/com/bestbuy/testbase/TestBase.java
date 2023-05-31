f283acd1ede524a659d5f325e5fb23892f50e81b
patelpinak <patelpinak30@gmail.com>
2023-06-01 00:22:14 +0100

    My week 17 homework BestBuy

---
+++ src/test/java/com/bestbuy/testbase/TestBase.java
@@ -0,0 +1,42 @@
+package com.bestbuy.testbase;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestBase {static ValidatableResponse response;
+
+    @BeforeClass
+    public static void inIt() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3030;
+        response = given().
+                when().
+                get("/stores").
+                then().statusCode(200);
+    }
+
+    //1. Extract the limit
+    @Test
+    public void test01() {
+        int limit = response.extract().path("limit");
+        System.out.println("------------------StartingTest---------------------------");
+        System.out.println("The value of limit is : " + limit);
+        System.out.println("------------------End of Test---------------------------");
+    }
+    @Test
+    public void test02() {
+
+
+
+    }
+
+
+
+
+
+
+}
